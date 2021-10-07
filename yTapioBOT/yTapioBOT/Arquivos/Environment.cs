457955f6d3f17f60e4f16f909f57d6d63615f426
Gabriel Trucollo <gabrieltrucollo@gmail.com>
2021-10-07 20:34:53 -0300

    Adicionado carregamento dos dados do .env

---
+++ yTapioBOT/yTapioBOT/Arquivos/Environment.cs
@@ -0,0 +1,85 @@
+﻿namespace yTapioBOT.Arquivos
+{
+    using System;
+    using System.Reflection;
+    using DotNetEnv;
+
+    /// <summary>
+    /// Classe Environment
+    /// </summary>
+    public class Environment
+    {
+        #region Propriedades
+        /// <summary>
+        /// Obtém ou define TwitchUserName
+        /// </summary>
+        public string TwitchUserName { get; set; }
+
+        /// <summary>
+        /// Obtém ou define TwitchAccountToken
+        /// </summary>
+        public string TwitchToken { get; set; }
+
+        /// <summary>
+        /// Obtém ou define DatabaseHost
+        /// </summary>
+        public string DatabaseHost { get; set; }
+
+        /// <summary>
+        /// Obtém ou define DatabaseHost
+        /// </summary>
+        public string DatabaseName { get; set; }
+
+        /// <summary>
+        /// Obtém ou define DatabaseHost
+        /// </summary>
+        public string DatabaseUser { get; set; }
+
+        /// <summary>
+        /// Obtém ou define DatabaseHost
+        /// </summary>
+        public string DatabasePassword { get; set; }
+
+        /// <summary>
+        /// Obtém ou define DatabaseHost
+        /// </summary>
+        public string DatabasePort { get; set; }
+        #endregion
+
+        #region Métodos
+        /// <summary>
+        /// Faz o carregamento dos dados do arquivo .env
+        /// </summary>
+        /// <returns>A classe Environment instânciada</returns>
+        public static Environment Carregar()
+        {
+            Environment env = new();
+
+            // Carregar .env
+            Env.Load();
+
+            // Atualizar classe com os dados do arquivo carregado
+            foreach (PropertyInfo propriedade in env.GetType().GetProperties())
+            {
+                try
+                {
+                    propriedade.SetValue(env, Env.GetString(propriedade.Name));
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine(string.Format(
+                        "Ocorreu um erro ao carregar os dados do .env{0}" +
+                        "Campo .:{1}{0}" +
+                        "Erro  .:{2}",
+                        System.Environment.NewLine,
+                        propriedade.Name,
+                        exp.Message));
+                }
+            }
+
+            // Retorno
+            return env;
+        }
+        #endregion
+    }
+}
