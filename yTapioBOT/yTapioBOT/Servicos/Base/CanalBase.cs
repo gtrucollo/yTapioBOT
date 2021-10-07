30b2a6cece6d52c91ef3fb0d56b485bcfede636c
Gabriel Trucollo <gabrieltrucollo@gmail.com>
2021-10-07 20:50:04 -0300

    Adicionado controles base de canal e serviço

---
+++ yTapioBOT/yTapioBOT/Servicos/Base/CanalBase.cs
@@ -0,0 +1,73 @@
+﻿namespace yTapioBOT.Servicos.Base
+{
+    using System;
+
+    /// <summary>
+    /// Classe CanalBase
+    /// </summary>
+    public abstract class CanalBase : IDisposable
+    {
+        #region Campos
+        /// <summary>
+        /// Controle para channel
+        /// </summary>
+        private readonly string channel;
+        #endregion
+
+        #region Construtor
+        /// <summary>
+        /// Inicia uma nova instância de <seealso cref="CanalBase"/>
+        /// </summary>
+        /// <param name="channel">Controle para o canal a ser conectado</param>
+        public CanalBase(string channel)
+        {
+            this.channel = channel;
+        }
+        #endregion
+
+        #region Propriedades
+        /// <summary>
+        /// Obtém Name
+        /// </summary>
+        public string Name => this.channel;
+        #endregion
+
+        #region Métodos
+        #region Dispose
+        /// <summary>
+        /// Método dispose
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Método dispose
+        /// </summary>
+        /// <param name="disposing">Controle para informar se está no dispose</param>
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposing)
+            {
+                return;
+            }
+        }
+        #endregion
+
+        #region Públicos
+        /// <summary>
+        /// Iniciar o serviço
+        /// </summary>
+        public abstract void Executar();
+
+        /// <summary>
+        /// Enviar mensagem no chat do serviço
+        /// </summary>
+        /// <param name="message">Mensagem a ser enviada</param>
+        public abstract void SendChannelMessage(string message);
+        #endregion
+        #endregion
+    }
+}
---
+++ yTapioBOT/yTapioBOT/Servicos/Base/ServicoBase.cs
@@ -0,0 +1,102 @@
+﻿namespace yTapioBOT.Servicos.Base
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Classe ServicoBase
+    /// </summary>
+    public abstract class ServicoBase : IDisposable
+    {
+        #region Campos
+        /// <summary>
+        /// Controle para usuario
+        /// </summary>
+        protected readonly string usuario;
+
+        /// <summary>
+        /// Controle para senha
+        /// </summary>
+        protected readonly string senha;
+
+        /// <summary>
+        /// Controle para o listChannel
+        /// </summary>
+        private IList<CanalBase> listChannel;
+        #endregion
+
+        #region Construtor
+        /// <summary>
+        /// Inicia uma nova instância de <seealso cref="ServicoBase"/>
+        /// </summary>
+        /// <param name="usuario">Controle para o usuario</param>
+        /// <param name="senha">Controle para a senha</param>
+        public ServicoBase(string usuario, string senha)
+        {
+            this.usuario = usuario;
+            this.senha = senha;
+        }
+        #endregion
+
+        #region Propriedades
+        /// <summary>
+        /// Obtém ou define ListChannel
+        /// </summary>
+        public IList<CanalBase> ListChannel
+        {
+            get
+            {
+                if (this.listChannel == null)
+                {
+                    this.listChannel = new List<CanalBase>();
+                }
+
+                return this.listChannel;
+            }
+
+            set
+            {
+                this.listChannel = value;
+            }
+        }
+        #endregion
+
+        #region Métodos
+        #region Dispose
+        /// <summary>
+        /// Método dispose
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+
+            // Liberar memória
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Método dispose
+        /// </summary>
+        /// <param name="disposing">Controle para informar se está no dispose</param>
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposing)
+            {
+                return;
+            }
+
+            // Disposing dos canais
+            this.ListChannel.ToList().ForEach(x => x.Dispose());
+        }
+        #endregion
+
+        #region Públicos
+        /// <summary>
+        /// Iniciar o serviço
+        /// </summary>
+        public abstract void Executar();
+        #endregion
+        #endregion
+    }
+}
