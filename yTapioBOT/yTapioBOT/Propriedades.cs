99d12ad87363c0f42788a2df75e1fffe22ac400c
Gabriel Trucollo <gabrieltrucollo@gmail.com>
2021-10-07 20:39:41 -0300

    Adicionado controle de propriedades estaticas

---
+++ yTapioBOT/yTapioBOT/Propriedades.cs
@@ -0,0 +1,37 @@
+﻿namespace yTapioBOT
+{
+    using Arquivos;
+
+    /// <summary>
+    /// Classe Propriedades
+    /// </summary>
+    public static class Propriedades
+    {
+        #region Campos
+        /// <summary>
+        /// Controle para env
+        /// </summary>
+        private static Environment env;
+        #endregion
+
+        #region Propriedades
+        /// <summary>
+        /// Obtém Environment
+        /// </summary>
+        public static Environment Env
+        {
+            get
+            {
+                // Carregar
+                if (env == null)
+                {
+                    env = Environment.Carregar();
+                }
+
+                // Retorno
+                return env;
+            }
+        }
+        #endregion
+    }
+}
