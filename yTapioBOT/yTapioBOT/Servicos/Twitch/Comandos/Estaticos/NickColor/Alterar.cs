b63c24efaa40850b33721e254e52e51535432561
Gabriel Trucollo <gabrieltrucollo@gmail.com>
2021-10-11 19:32:39 -0300

    Adicionado opção de mudar a cor do nick do bot

---
+++ yTapioBOT/yTapioBOT/Servicos/Twitch/Comandos/Estaticos/NickColor/Alterar.cs
@@ -0,0 +1,55 @@
+﻿namespace yTapioBOT.Servicos.Twitch.Comandos.Estaticos.Banimento
+{
+    using System.Linq;
+    using Base.Comandos.Estaticos;
+
+    /// <summary>
+    /// Classe Alterar
+    /// </summary>
+    [Nome("c")]
+    public sealed class Alterar : AdicionarBase
+    {
+        #region Construtor
+        /// <summary>
+        /// Incia uma nova instância de <seealso cref="Adicionar"/>
+        /// </summary>
+        /// <param name="canal">Objeto com as informações do canal</param>
+        /// <param name="argumentos">Relação de argumentos do comando</param>
+        public Alterar(Canal canal, params string[] argumentos)
+            : base(canal, argumentos)
+        {
+        }
+        #endregion
+
+        #region Propriedades
+        /// <summary>
+        /// Obtém Cor
+        /// </summary>
+        private string Cor
+        {
+            get
+            {
+                return this.Argumentos.FirstOrDefault();
+            }
+        }
+        #endregion
+
+        #region Métodos
+        #region Públicos
+        /// <inheritdoc/>
+        public override void Executar()
+        {
+            // Validar
+            if (string.IsNullOrWhiteSpace(this.Cor))
+            {
+                this.Canal.SendChannelMessage("Argumentos inválidos, não foi informado o a cor a ser utilizada no meu nick.");
+                return;
+            }
+
+            // Executar
+            this.Canal.SendChannelMessage(string.Format("/color {0}", this.Cor));
+        }
+        #endregion
+        #endregion
+    }
+}
